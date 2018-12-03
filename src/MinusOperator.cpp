c2caf7bab335c04f3f0c2ec1f02dae80d37be99e
Taka Koutsomitopoulos <vasilis.koutsomitopoulos4p@stu.austinisd.org>
2018-12-03 10:27:44 -0600

    Fixed MinusOperator - made the imaginary components subtract

--- src/MinusOperator.cpp
+++ src/MinusOperator.cpp
@@ -1,9 +1,9 @@
-#include "ComplexMath.h"
+#include "COMPLEXMATH.h"
 using namespace std;
 
 Complex Complex:: operator- (Complex* rhs){
    Complex finalComplex = *rhs;
    finalComplex.re = this->re + rhs->re;
-   finalComplex.re = this->re + rhs->re;
+   finalComplex.im = this->im - rhs->im;
    return finalComplex;
 }
