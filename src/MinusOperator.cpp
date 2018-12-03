36f754ae271a8d781a9144d0f30e7261f0c4e9e8
Taka Koutsomitopoulos <vasilis.koutsomitopoulos4p@stu.austinisd.org>
2018-12-03 10:30:11 -0600

    Fixed MinusOperator - real components subtract as well

--- src/MinusOperator.cpp
+++ src/MinusOperator.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 Complex Complex:: operator- (Complex* rhs){
    Complex finalComplex = *rhs;
-   finalComplex.re = this->re + rhs->re;
+   finalComplex.re = this->re - rhs->re;
    finalComplex.im = this->im - rhs->im;
    return finalComplex;
 }
