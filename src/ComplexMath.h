5d23935e40c115fd5910a0f4d6d631533fbf5c47
Taka Koutsomitopoulos <vasilis.koutsomitopoulos4p@stu.austinisd.org>
2018-12-04 15:04:11 -0600

    Added declaration for the Complex::conj() function

--- src/ComplexMath.h
+++ src/ComplexMath.h
@@ -9,7 +9,7 @@ class Complex {
 	double re, im;
    friend ostream& operator<<(ostream&, const Complex&);
    friend istream& operator>>(istream&, const Complex&);
-   
+
 	public:
 		Complex();
 		Complex(double r, double i=0.0);
@@ -22,9 +22,10 @@ class Complex {
       void operator*= (Complex* rhs);
       Complex operator/ (Complex* rhs);
       void operator/= (Complex* rhs);
-      
+
       double abs();
       double arg();
+			Complex conj();
 };
 
 #endif
