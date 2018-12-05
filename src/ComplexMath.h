373f8c47587b3caf4b1b992801cbeedae3bf0b61
Griffin Davis <griffincdavis@gmail.com>
2018-12-05 09:49:28 -0600

    Update ComplexMath.h

--- src/ComplexMath.h
+++ src/ComplexMath.h
@@ -6,26 +6,26 @@
 using namespace std;
 
 class Complex {
-	double re, im;
+   double re, im;
    friend ostream& operator<<(ostream&, const Complex&);
    friend istream& operator>>(istream&, const Complex&);
 
 	public:
-      Complex();
-      Complex(double r, double i=0.0);
-      void operator= (Complex* rhs);
-      Complex operator+ (Complex* rhs);
-      void operator+= (Complex* rhs);
-      Complex operator- (Complex* rhs);
-      void operator-= (Complex* rhs);
-      Complex operator* (Complex* rhs);
-      void operator*= (Complex* rhs);
-      Complex operator/ (Complex* rhs);
-      void operator/= (Complex* rhs);
+   Complex();
+   Complex(double r, double i=0.0);
+   void operator= (Complex* rhs);
+   Complex operator+ (Complex* rhs);
+   void operator+= (Complex* rhs);
+   Complex operator- (Complex* rhs);
+   void operator-= (Complex* rhs);
+   Complex operator* (Complex* rhs);
+   void operator*= (Complex* rhs);
+   Complex operator/ (Complex* rhs);
+   void operator/= (Complex* rhs);
 
-      double abs();
-      double arg();
-		Complex conj();
+   double abs();
+   double arg();
+   Complex conj();
 };
 
 #endif
