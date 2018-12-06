93800c4873a5d34598563dad7a1ada8590f71bd2
danikaluo <45433190+danikaluo@users.noreply.github.com>
2018-12-05 21:23:44 -0600

    Wrote + and += operators

---
+++ PlusOperator.cpp
@@ -0,0 +1,9 @@
+#include "COMPLEXMATH.h"
+using namespace std;
+
+Complex Complex:: operator+ (Complex* rhs){
+   Complex finalComplex = *rhs;
+   finalComplex.re = this->re + rhs->re;
+   finalComplex.im = this->im + rhs->im;
+   return finalComplex;
+}
