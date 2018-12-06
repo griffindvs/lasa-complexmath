48777b4d044f2897626c79703dde43aa77bbf51a
Griffin Davis <griffincdavis@gmail.com>
2018-12-06 09:30:17 -0600

    Build Library and Update ComplexMath.h

--- src/ComplexMath.h
+++ src/ComplexMath.h
@@ -7,25 +7,24 @@ using namespace std;
 
 class Complex {
    double re, im;
-   friend ostream& operator<<(ostream&, const Complex&);
-   friend istream& operator>>(istream&, const Complex&);
+   friend ostream& operator<<(ostream& lhs, const Complex& rhs);
+   friend Complex operator>>(istream& lhs, Complex& rhs);
+   friend double abs(Complex c);
+   friend double arg(Complex c);
+   friend Complex conj(Complex c);
 
 	public:
    Complex();
    Complex(double r, double i=0.0);
    void operator= (Complex* rhs);
-   Complex operator+ (Complex* rhs);
+   Complex operator+ (Complex& rhs);
    void operator+= (Complex* rhs);
    Complex operator- (Complex* rhs);
    void operator-= (Complex* rhs);
-   Complex operator* (Complex* rhs);
+   Complex operator* (Complex& rhs);
    void operator*= (Complex* rhs);
    Complex operator/ (Complex* rhs);
    void operator/= (Complex* rhs);
-
-   double abs();
-   double arg();
-   Complex conj();
 };
 
 #endif
