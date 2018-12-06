fc0ad414019e30c5fe3db354818cb7bdd4a63424
danikaluo <45433190+danikaluo@users.noreply.github.com>
2018-12-05 21:27:03 -0600

    Add PlusOperator.cpp and PlusEqualsOperator.cpp

---
+++ src/PlusEqualsOperator.cpp
@@ -0,0 +1,8 @@
+#include "COMPLEXMATH.h"
+using namespace std;
+
+void Complex:: operator+= (Complex* rhs){
+   Complex initialComplex = *this;
+   initialComplex = initialComplex + rhs;
+   *this = initialComplex;
+}
