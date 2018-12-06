b546cee799f7a6c546bf6e542173edfa84bec478
danikaluo <45433190+danikaluo@users.noreply.github.com>
2018-12-05 21:25:09 -0600

    Delete PlusOperator.cpp

--- PlusOperator.cpp
+++
@@ -1,9 +0,0 @@
-#include "COMPLEXMATH.h"
-using namespace std;
-
-Complex Complex:: operator+ (Complex* rhs){
-   Complex finalComplex = *rhs;
-   finalComplex.re = this->re + rhs->re;
-   finalComplex.im = this->im + rhs->im;
-   return finalComplex;
-}
