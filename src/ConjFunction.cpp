7ea00369d38832740ea7bc018235320d5e6e4c33
Griffin Davis <griffincdavis@gmail.com>
2018-12-06 09:31:03 -0600

    Update ConjFunction.cpp

--- src/ConjFunction.cpp
+++ src/ConjFunction.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-Complex Complex::conj(){
-  double realComponent = this->re;
-  double imaginaryComponent = this->im;
+Complex conj(Complex c){
+  double realComponent = c.re;
+  double imaginaryComponent = c.im;
   Complex complexConjugate(realComponent, imaginaryComponent * -1);
   return complexConjugate;
 }
