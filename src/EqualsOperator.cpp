fd718a3d6e0f8d318a0cd75dbac9e56f04200d6d
takakoutso <taka.koutso@gmail.com>
2018-12-07 10:35:18 -0600

    Something happened idk

--- src/EqualsOperator.cpp
+++ src/EqualsOperator.cpp
@@ -1,9 +1,9 @@
 #include "COMPLEXMATH.h"
 using namespace std;
 
-void Complex:: operator= (Complex* rhs){
+void Complex:: operator= (Complex& rhs){
    Complex finalComplex = *this;
-   finalComplex.re = rhs->re;
-   finalComplex.im = rhs->im;
+   finalComplex.re = rhs.re;
+   finalComplex.im = rhs.im;
    *this = finalComplex;
 }
