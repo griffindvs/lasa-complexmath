0179c242dd25b8ba5e3d4f8658d2d0c47e97c6b3
takakoutso <taka.koutso@gmail.com>
2018-12-07 10:38:17 -0600

    More things

--- src/EqualsOperator.cpp
+++ src/EqualsOperator.cpp
@@ -1,7 +1,7 @@
 #include "COMPLEXMATH.h"
 using namespace std;
 
-void Complex:: operator= (Complex& rhs){
+void Complex:: operator= (const Complex& rhs){
    Complex finalComplex = *this;
    finalComplex.re = rhs.re;
    finalComplex.im = rhs.im;
