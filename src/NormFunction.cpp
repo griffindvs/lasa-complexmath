982d4f6268f0894aa0c5c908e3f5c556b4b4eacd
Griffin Davis <griffincdavis@gmail.com>
2018-12-07 10:49:42 -0600

    Update NormFunction.cpp

--- src/NormFunction.cpp
+++ src/NormFunction.cpp
@@ -1,13 +1,4 @@
-/*
-Norm Function
-Danika Luo
-Created 12/7/18
-Desc: Returns the square of the magnitude of its argument.
-Modified: 12/7/18
-*/
-
 #include "COMPLEXMATH.h"
-#include <cmath>
 
 using namespace std;
 
