afa0b31232225f3a15b69fb23e8fc0ec648fbffc
Griffin Davis <griffincdavis@gmail.com>
2018-12-08 15:22:19 -0600

    Update ExtractOperator.cpp

--- src/ExtractOperator.cpp
+++ src/ExtractOperator.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 istream& operator>> (istream& lhs, Complex& c){
-   if (c.im) {
-      lhs >> c.re >> c.im;
-   } else {
-      lhs >> c.re;
-   }
+   string a;
+   lhs >> a;
+   double re;
+   double im;
+   int i = a.find(',');
+   re = atof(a.substr(1,i-1).c_str());
+   im = atof(a.substr(i+1, a.length()-2-i).c_str());
+   
+   Complex b(re,im);
+   c = b;
+   
    return lhs;
 }
