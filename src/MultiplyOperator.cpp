3be5cd96f1fad050e3210d456ed2b16e2d6bbe84
Travis Eaves <travis.eaves14@stu.austinisd.org>
2018-12-10 10:46:40 -0600

    inline comments

--- src/MultiplyOperator.cpp
+++ src/MultiplyOperator.cpp
@@ -12,7 +12,7 @@ using namespace std;
 Complex Complex::operator* (const Complex& rhs){
    Complex product;
    //distribution and simplification of two complex numbers
-   product.re = (this->re * rhs.re) + (-1 * (this->im * rhs.im));
+   product.re = (this->re * rhs.re) + (-1 * (this->im * rhs.im));//i^2 = -1
    product.im = (this->re * rhs.im) + (this->im * rhs.re);
    return product;
 }
