e48a88383bd135c6ed910cdc9db11c79befdadd7
Griffin Davis <griffincdavis@gmail.com>
2018-12-10 10:26:42 -0600

    Add comments

--- src/ArgFunction.cpp
+++ src/ArgFunction.cpp
@@ -1,13 +1,21 @@
+/* 
+Title : ArgFunction
+Author : Griffin Davis
+Description : Returns the angle of the Complex number.
+Created : 12/3/18
+Modified : 12/8/18 
+*/
+
 #include "COMPLEXMATH.h"
 
 using namespace std;
 
 double arg(Complex c){
    double y, x;
-   y = c.im;
-   x = c.re;
+   y = c.im; //Sets temporary value for imaginary component
+   x = c.re; //Sets temporary value for real component
 
    double angle;
-   angle = atan(y/x);
-   return angle;
+   angle = atan(y/x); //Calculates the arctangent of the imaginary/real
+   return angle; //Returns the calculated angle
 }
--- src/InsertOperator.cpp
+++ src/InsertOperator.cpp
@@ -1,8 +1,16 @@
+/* 
+Title : InsertOperator
+Author : Griffin Davis
+Description : Overloads the insertion operator for the Complex class.
+Created : 12/3/18
+Modified : 12/7/18 
+*/
+
 #include "COMPLEXMATH.h"
 
 using namespace std;
 
 ostream& operator<< (ostream& lhs, const Complex& rhs){
-   lhs << "(" << rhs.re << ", " << rhs.im << ")";
+   lhs << "(" << rhs.re << ", " << rhs.im << ")"; //Handles the printing of each component in (re, im) format
    return lhs;
 }
