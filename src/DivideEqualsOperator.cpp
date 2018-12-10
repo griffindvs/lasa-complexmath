56820a60ffa5eaf150865df2c02346f8791077fc
Travis Eaves <travis.eaves14@stu.austinisd.org>
2018-12-10 11:02:31 -0600

    TJ's comments

--- src/DivideEqualsOperator.cpp
+++ src/DivideEqualsOperator.cpp
@@ -1,9 +1,16 @@
+/*
+Title: DivideEqualsOperator
+Author: TJ Arora
+Description: operator overload of /= for complex numbers
+Created: 12/7/18
+Modified: 12/10/18
+*/
 #include "COMPLEXMATH.h"
 
 using namespace std;
 
 void Complex::operator/= (const Complex& rhs){
    Complex initialComplex = *this;
-   initialComplex = initialComplex / rhs;
+   initialComplex = initialComplex / rhs;//overloaded / operator for complex
    *this = initialComplex;
 }
--- src/DivideOperator.cpp
+++ src/DivideOperator.cpp
@@ -1,3 +1,10 @@
+/*
+Title: DivideOperator
+Author: TJ Arora
+Description: operator overload of / for complex numbers
+Created: 12/7/18
+Modified: 12/10/18
+*/
 #include "COMPLEXMATH.h"
 
 using namespace std;
@@ -5,7 +12,8 @@ using namespace std;
 Complex Complex::operator/ (const Complex& rhs) {
     Complex finalComplex;
 
-    double divisor = (rhs.re * rhs.re) + (rhs.im * rhs.im);
+    double divisor = (rhs.re * rhs.re) + (rhs.im * rhs.im);//distribution and simplification of denominator after being multiplied by conjugate
+    //distribution and simplification of numerator
     double num_re = (this->re * rhs.re) + (this->im * rhs.im);
     double num_img = (this->im * rhs.re) - (this->re * rhs.im);
 
